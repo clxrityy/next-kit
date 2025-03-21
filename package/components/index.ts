86b957558f799d14e6a1d29e13203c9f7f44c7b9
clxrityy <clxrityxyz@gmail.com>
2025-03-20 20:06:32 -0400

    import & exports

---
+++ package/components/index.ts
@@ -0,0 +1,5 @@
+import { Image, type ImageProps } from "./lib/Image";
+export { Image, type ImageProps };
+
+import { Skeleton, type SkeletonProps } from "./lib/Skeleton";
+export { Skeleton, type SkeletonProps };
