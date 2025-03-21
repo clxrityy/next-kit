73014f29085e510302ded41a2144d97cd80b10f6
clxrityy <clxrityxyz@gmail.com>
2025-03-20 20:07:06 -0400

    add cn

--- package/utils/index.ts
+++ package/utils/index.ts
@@ -1,5 +1,7 @@
 import type { ContextFactory } from "./lib/contextFactory";
 import { contextFactory } from "./lib/contextFactory";
-
 export type { ContextFactory };
 export { contextFactory };
+
+import { cn } from "./lib/cn";
+export { cn };
